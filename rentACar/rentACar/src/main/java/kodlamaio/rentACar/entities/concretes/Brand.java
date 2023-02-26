0165b8054ae4cbd583fc0398da23e369309d5305
Özge Odabaş <180315051@ogr.cbu.edu.tr>
2023-02-26 23:18:06 +0300

    lombok used for brand entity

--- rentACar/rentACar/src/main/java/kodlamaio/rentACar/entities/concretes/Brand.java
+++ rentACar/rentACar/src/main/java/kodlamaio/rentACar/entities/concretes/Brand.java
@@ -1,33 +1,29 @@
 package kodlamaio.rentACar.entities.concretes;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="brands")
 public class Brand {
+	
+	@Column(name="id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	@Column(name="name")
 	private String name;
-	
-	public Brand() {
-	}
-
-	public Brand(int id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	
 	
 }
